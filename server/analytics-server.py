38d6f28f7ba037f0f1e5b0a9b82bd84c4ee02a12
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2018-11-03 13:42:42 -0700

    Web server fixes

--- server/analytics-server.py
+++ server/analytics-server.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 @app.route("/coq-analytics/", methods=["POST"])
 def log_command():
     with open(logpath, 'a') as logfile:
-        logfile.write("{}: {}".format(request.form["timestamp"],
-                                      request.form["command"]))
+        logfile.write("{}: {}\n".format(request.form["timestamp"],
+                              	        request.form["command"]))
     return 'Submitted'
--- server/start-server.sh
+++ server/start-server.sh
@@ -1,1 +1,1 @@
-FLASK_APP=analytics-server.py flask run
+FLASK_APP=analytics-server.py flask run --host=0.0.0.0 --port=61234
