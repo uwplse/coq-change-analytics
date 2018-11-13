7f344134a1760d448e0fa9263ea1fb6204f95366
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2018-11-13 14:33:07 -0800

    Handle multiple messages in each sexp

--- server/analytics-server.py
+++ server/analytics-server.py
@@ -10,12 +10,13 @@ app = Flask(__name__)
 @app.route("/coq-analytics/", methods=["POST"])
 def log_command():
     try:
-        sexp = [[Symbol("user"), str(request.remote_addr)]] + loads(request.form["msg"])
+        all_sexps = loads(request.form["msg"])
+        with open(logpath, 'a') as logfile:
+            for sexp in all_sexps:
+                dump([[Symbol("user"), str(request.remote_addr)]] + sexp, logfile)
+                logfile.write("\n")
     except:
         print("Got bad plugin message: {}".format(request.form["msg"]))
         print("Ignoring...")
         return "Failed"
-    with open(logpath, 'a') as logfile:
-        dump(sexp, logfile)
-        logfile.write("\n")
     return 'Submitted'
