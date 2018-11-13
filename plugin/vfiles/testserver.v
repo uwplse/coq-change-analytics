b083ed47a8f3beb201ec371333be5bf2eacf80dd
Talia Ringer <tringer@cs.washington.edu>
2018-11-13 14:53:13 -0800

    don't commit unusued code

--- plugin/vfiles/testserver.v
+++ plugin/vfiles/testserver.v
@@ -1,10 +1,5 @@
 Require Import Analytical.Analytics.
 
-Require Import Nat.
-Theorem nat_refl : forall x : nat, x = x.
-Proof.
-reflexivity.
-Qed.
 (*
  * To test the plugin, write and modify Coq code of your choice here
  * using your favorite IDE.
