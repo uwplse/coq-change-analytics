d7637efde59d8c0200ed431ef9c401a2cc58a804
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2019-05-22 10:25:34 -0700

    Better build.sh for buildling coq makefile

--- plugin/build.sh
+++ plugin/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 (cd coq && ./configure -local && make -j 5)
-coq_makefile -f _CoqProject CAMLPKGS = "-package cohttp -package cohttp-lwt-unix -package lwt" -o Makefile
+./coq/bin/coq_makefile -f _CoqProject CAMLPKGS = "-package cohttp -package cohttp-lwt-unix -package lwt" -o Makefile
