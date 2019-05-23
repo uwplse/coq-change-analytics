938db9be0e7c7f56aab953429db760aaef3e1334
Talia Ringer <tringer@cs.washington.edu>
2019-05-23 18:22:09 -0400

    add submodule syncing to build.sh

--- plugin/build.sh
+++ plugin/build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+git submodule init
+git submodule sync
+git submodule update --init --recursive --remote
+
 (cd coq && ./configure -local && make -j 5)
 ./coq/bin/coq_makefile -f _CoqProject -o Makefile
