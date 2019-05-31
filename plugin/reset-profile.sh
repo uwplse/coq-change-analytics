051c7cc4401dc010f1f6d1e63417cb78c9b19bb1
Talia Ringer <tringer@cs.washington.edu>
2019-05-31 16:37:45 -0400

    move back to actual server

--- plugin/reset-profile.sh
+++ plugin/reset-profile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 profile="$HOME/.analytics_profile"
 uid=$(cat $profile)
-curl -X POST -F "id=$uid" http://localhost:4444/reset-profile/
+curl -X POST -F "id=$uid" http://ec2-18-225-35-143.us-east-2.compute.amazonaws.com:44/reset-profile/
