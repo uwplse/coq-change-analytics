f6ca58444c2cb41b914781bf0492905c7eca2318
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2019-06-14 17:38:21 -0700

    Some fixes to python code (2 -> 3)

--- server/analytics-server.py
+++ server/analytics-server.py
@@ -31,47 +31,45 @@ def log_command():
 @app.route("/register/", methods=["POST"])
 def register():
     try:
-        users = open(userpath, 'r')
-        profiles = load(users)
-        new_uid = str(len(profiles))
-        users.close()
+        with open(userpath, 'r') as f:
+            users = f.read()
+            profiles = loads(users)
+            new_uid = str(len(profiles))
     except:
         profiles = []
         new_uid = "0"
     finally:
-        users = open(userpath, 'w')
-        dump(profiles + [[[Symbol("user"), new_uid]] + [[Symbol("version"), "0"]]], users)
-        users.write("\n")
-        users.close()
+        with open(userpath, 'w') as f:
+            f.write(dump(profiles + [[[Symbol("user"), new_uid]] + [[Symbol("version"), "0"]]]) + "\n")
         return str(new_uid)
 
 # Determine if a profile is up-to-date and, if not, ask the latest profile questions
 @app.route("/sync-profile/", methods=["GET"])
 def sync_profile():
     uid = int(request.args.get('id'))
-    users = open(userpath, 'r')
-    profile = load(users)[uid]
-    users.close()
+    with open(userpath, 'r') as f:
+        users = f.read()
+        profile = loads(users)[uid]
     version = profile[1][1]
     if version == version_id:
         return dumps([])
     else:
-        question_store = open(questionpath, 'r')
-        questions = dumps(load(question_store))
+        with open(questionpath, 'r') as qf:
+            question_store = qf.read()
+            questions = dumps(loads(question_store))
         return questions
 
 # Reset a profile
 @app.route("/reset-profile/", methods=["POST"])
 def reset_profile():
     uid = request.form["id"]
-    users = open(userpath, 'r')
-    profiles = load(users)
-    users.close()
-    users = open(userpath, 'w')
-    new_profile = [[Symbol("user"), uid]] + [[Symbol("version"), "0"]]
-    profiles[int(uid)] = new_profile
-    dump(profiles, users) 
-    users.close()
+    with open(userpath, 'r') as f:
+        users = f.read()
+        profiles = loads(users)
+    with open(userpath, 'w') as f:
+        new_profile = [[Symbol("user"), uid]] + [[Symbol("version"), "0"]]
+        profiles[int(uid)] = new_profile
+        f.write(dump(profiles))
     return "Succesfully reset\n"
 
 # Update a profile with answers to the profile questions
@@ -79,13 +77,13 @@ def reset_profile():
 def update_profile():
     uid = request.form["id"]
     answers = loads(request.form["answers"]) # list of 0-indexed offsets of answers for each question
-    users = open(userpath, 'r')
-    profiles = load(users)
-    users.close()
-    users = open(userpath, 'w')
-    new_profile = [[Symbol("user"), uid]] + [[Symbol("version"), version_id]] + [[Symbol("answers"), answers]]
-    profiles[int(uid)] = new_profile
-    dump(profiles, users) 
-    users.close()
+    with open(userpath, 'r') as f:
+        users = f.read()
+        print(users)
+        profiles = loads(users)
+    with open(userpath, 'w') as f:
+        new_profile = [[Symbol("user"), uid]] + [[Symbol("version"), version_id]] + [[Symbol("answers"), answers]]
+        profiles[int(uid)] = new_profile
+        f.write(users)
     return "Updated"
 
